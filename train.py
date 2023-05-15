36ea4b63e20d0ec5cd284b833178cd086030b5c8
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-15 16:51:55 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,58 +1,41 @@
 import streamlit as st
 import subprocess
-import json
 
-# 输入 OpenAI API 密钥
-api_key = st.text_input("Enter OpenAI API Key")
-if not api_key:
-    st.warning("Please enter your OpenAI API Key")
+# 让用户输入 OpenAI API 密钥
+api_key = st.text_input("请输入您的 OpenAI API 密钥")
 
-# 获取以前的模型列表
-command = ["openai", "api", "fine_tunes.list"]
-process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-stdout, stderr = process.communicate()
+# 检查 API 密钥的格式是否正确
+if not api_key.startswith("YOUR_OPENAI_API_KEY_PREFIX"):
+    st.error("API 密钥格式不正确")
 
-# 解析命令输出
-if stderr:
-    st.error(stderr.decode())
-    models = []
-else:
-    models = json.loads(stdout.decode())
+# 创建一个文本区域用于显示终端输出
+terminal_output = st.text_area("Terminal 输出", value="", height=200)
 
-# 提取模型名称列表
-model_names = [model["display_name"] for model in models]
+# 定义一个函数，用于将终端输出添加到文本区域中
+def append_to_terminal_output(text):
+    terminal_output += text
+    st.text_area("Terminal 输出", value=terminal_output, height=200)
 
-# 添加系统默认模型
-model_names.append("System Default")
-
-# 创建一个下拉选择框，供用户选择模型
-selected_model = st.selectbox("选择要训练的模型", model_names)
-
-# 让用户上传.jsonl文件
-uploaded_file = st.file_uploader("上传.jsonl文件", type="jsonl")
-
-if uploaded_file:
-    # 将文件保存到本地
-    with open(uploaded_file.name, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-
+# 当用户点击“开始训练”按钮时触发的事件
+if st.button("开始训练"):
     # 使用CLI指令训练模型
     command = [
         "openai",
         "api",
         "fine_tunes.create",
         "-t",
-        uploaded_file.name,
+        "TRAIN_FILE_ID_OR_PATH",
         "-m",
-        selected_model
+        "BASE_MODEL"
     ]
 
-    # 执行命令并捕获输出
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
+    # 执行命令并逐行读取输出
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+    for line in process.stdout:
+        append_to_terminal_output(line)
+
+    # 等待命令执行完成
+    process.wait()
 
     # 显示终端输出的文本
-    if stderr:
-        st.error(stderr.decode())
-    else:
-        st.code(stdout.decode())
+    append_to_terminal_output("命令执行完成。退出码：" + str(process.returncode))
